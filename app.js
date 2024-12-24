ee77b0684eb50eca689b21e2d22dc6cdf074113d
saifahmad02 <sahma244@uwo.ca>
2024-12-24 18:07:56 -0500

    Importing files from local development folder

---
+++ app.js
@@ -0,0 +1,631 @@
+const countryList = [
+    {
+        name: "Afghanistan"
+    },
+    {
+        name: "Albania"
+    },
+    {
+        name: "Algeria"
+    },
+    {
+        name: "Andorra"
+    },
+    {
+        name: "Angola"
+    },
+    {
+        name: "Antigua and Barbuda"
+    },
+    {
+        name: "Argentina"
+    },
+    {
+        name: "Armenia"
+    },
+    {
+        name: "Austria"
+    },
+    {
+        name: "Azerbaijan"
+    },
+    {
+        name: "Bahrain"
+    },
+    {
+        name: "Bangladesh"
+    },
+    {
+        name: "Barbados"
+    },
+    {
+        name: "Belarus"
+    },
+    {
+        name: "Belgium"
+    },
+    {
+        name: "Belize"
+    },
+    {
+        name: "Benin"
+    },
+    {
+        name: "Bhutan"
+    },
+    {
+        name: "Bolivia"
+    },
+    {
+        name: "Bosnia and Herzegovina"
+    },
+    {
+        name: "Botswana"
+    },
+    {
+        name: "Brazil"
+    },
+    {
+        name: "Brunei"
+    },
+    {
+        name: "Bulgaria"
+    },
+    {
+        name: "Burkina Faso"
+    },
+    {
+        name: "Burundi"
+    },
+    {
+        name: "Cabo Verde"
+    },
+    {
+        name: "Cambodia"
+    },
+    {
+        name: "Cameroon"
+    },
+    {
+        name: "Canada"
+    },
+    {
+        name: "Central African Republic"
+    },
+    {
+        name: "Chad"
+    },
+    {
+        name: "Chile"
+    },
+    {
+        name: "Colombia"
+    },
+    {
+        name: "Comoros"
+    },
+    {
+        name: "Congo"
+    },
+    {
+        name: "Costa Rica"
+    },
+    {
+        name: "Croatia"
+    },
+    {
+        name: "Cuba"
+    },
+    {
+        name: "Cyprus"
+    },
+    {
+        name: "Czech Republic"
+    },
+    {
+        name: "Denmark"
+    },
+    {
+        name: "Djibouti"
+    },
+    {
+        name: "Dominica"
+    },
+    {
+        name: "Dominican Republic"
+    },
+    {
+        name: "Congo"
+    },
+    {
+        name: "Ecuador"
+    },
+    {
+        name: "Egypt"
+    },
+    {
+        name: "El Salvador"
+    },
+    {
+        name: "Equatorial Guinea"
+    },
+    {
+        name: "Eritrea"
+    },
+    {
+        name: "Estonia"
+    },
+    {
+        name: "Eswatini"
+    },
+    {
+        name: "Ethiopia"
+    },
+    {
+        name: "Finland"
+    },
+    {
+        name: "France"
+    },
+    {
+        name: "French Guiana"
+    },
+    {
+        name: "Gabon"
+    },
+    {
+        name: "Gambia"
+    },
+    {
+        name: "Georgia"
+    },
+    {
+        name: "Germany"
+    },
+    {
+        name: "Ghana"
+    },
+    {
+        name: "Gibraltar"
+    },
+    {
+        name: "Greece"
+    },
+    {
+        name: "Grenada"
+    },
+    {
+        name: "Guatemala"
+    },
+    {
+        name: "Guinea"
+    },
+    {
+        name: "Guinea-Bissau"
+    },
+    {
+        name: "Guyana"
+    },
+    {
+        name: "Haiti"
+    },
+    {
+        name: "Holy See"
+    },
+    {
+        name: "Honduras"
+    },
+    {
+        name: "Hong Kong"
+    },
+    {
+        name: "Hungary"
+    },
+    {
+        name: "Iceland"
+    },
+    {
+        name: "India"
+    },
+    {
+        name: "Indonesia"
+    },
+    {
+        name: "Iran"
+    },
+    {
+        name: "Iraq"
+    },
+    {
+        name: "Ireland"
+    },
+    {
+        name: "Isle of Man"
+    },
+    {
+        name: "Israel"
+    },
+    {
+        name: "Italy"
+    },
+    {
+        name: "Jamaica"
+    },
+    {
+        name: "Japan"
+    },
+    {
+        name: "Jordan"
+    },
+    {
+        name: "Kazakhstan"
+    },
+    {
+        name: "Kenya"
+    },
+    {
+        name: "Kuwait"
+    },
+    {
+        name: "Kyrgyzstan"
+    },
+    {
+        name: "Laos"
+    },
+    {
+        name: "Latvia"
+    },
+    {
+        name: "Lebanon"
+    },
+    {
+        name: "Lesotho"
+    },
+    {
+        name: "Liberia"
+    },
+    {
+        name: "Libya"
+    },
+    {
+        name: "Liechtenstein"
+    },
+    {
+        name: "Lithuania"
+    },
+    {
+        name: "Luxembourg"
+    },
+    {
+        name: "Macao"
+    },
+    {
+        name: "Madagascar"
+    },
+    {
+        name: "Malawi"
+    },
+    {
+        name: "Malaysia"
+    },
+    {
+        name: "Maldives"
+    },
+    {
+        name: "Mali"
+    },
+    {
+        name: "Malta"
+    },
+    {
+        name: "Mauritania"
+    },
+    {
+        name: "Mauritius"
+    },
+    {
+        name: "Mayotte"
+    },
+    {
+        name: "Mexico"
+    },
+    {
+        name: "Moldova"
+    },
+    {
+        name: "Monaco"
+    },
+    {
+        name: "Mongolia"
+    },
+    {
+        name: "Montenegro"
+    },
+    {
+        name: "Morocco"
+    },
+    {
+        name: "Mozambique"
+    },
+    {
+        name: "Myanmar"
+    },
+    {
+        name: "Namibia"
+    },
+    {
+        name: "Nepal"
+    },
+    {
+        name: "Netherlands"
+    },
+    {
+        name: "Nicaragua"
+    },
+    {
+        name: "Niger"
+    },
+    {
+        name: "Nigeria"
+    },
+    {
+        name: "North Korea"
+    },
+    {
+        name: "North Macedonia"
+    },
+    {
+        name: "Norway"
+    },
+    {
+        name: "Oman"
+    },
+    {
+        name: "Pakistan"
+    },
+    {
+        name: "Panama"
+    },
+    {
+        name: "Paraguay"
+    },
+    {
+        name: "Peru"
+    },
+    {
+        name: "Philippines"
+    },
+    {
+        name: "Poland"
+    },
+    {
+        name: "Portugal"
+    },
+    {
+        name: "Qatar"
+    },
+    {
+        name: "Romania"
+    },
+    {
+        name: "Russia"
+    },
+    {
+        name: "Rwanda"
+    },
+    {
+        name: "Saint Helena"
+    },
+    {
+        name: "Saint Kitts and Nevis"
+    },
+    {
+        name: "Saint Lucia"
+    },
+    {
+        name: "Saint Vincent and the Grenadines"
+    },
+    {
+        name: "San Marino"
+    },
+    {
+        name: "Saudi Arabia"
+    },
+    {
+        name: "Senegal"
+    },
+    {
+        name: "Serbia"
+    },
+    {
+        name: "Seychelles"
+    },
+    {
+        name: "Sierra Leone"
+    },
+    {
+        name: "Singapore"
+    },
+    {
+        name: "Slovakia"
+    },
+    {
+        name: "Slovenia"
+    },
+    {
+        name: "Somalia"
+    },
+    {
+        name: "South Africa"
+    },
+    {
+        name: "South Korea"
+    },
+    {
+        name: "South Sudan"
+    },
+    {
+        name: "Spain"
+    },
+    {
+        name: "Sri Lanka"
+    },
+    {
+        name: "Palestine"
+    },
+    {
+        name: "Sudan"
+    },
+    {
+        name: "Suriname"
+    },
+    {
+        name: "Sweden"
+    },
+    {
+        name: "Switzerland"
+    },
+    {
+        name: "Syria"
+    },
+    {
+        name: "Taiwan"
+    },
+    {
+        name: "Tajikistan"
+    },
+    {
+        name: "Tanzania"
+    },
+    {
+        name: "Thailand"
+    },
+    {
+        name: "The Bahamas"
+    },
+    {
+        name: "Timor-Leste"
+    },
+    {
+        name: "Togo"
+    },
+    {
+        name: "Trinidad and Tobago"
+    },
+    {
+        name: "Tunisia"
+    },
+    {
+        name: "Turkey"
+    },
+    {
+        name: "Turkmenistan"
+    },
+    {
+        name: "Uganda"
+    },
+    {
+        name: "Ukraine"
+    },
+    {
+        name: "United Arab Emirates"
+    },
+    {
+        name: "United Kingdom"
+    },
+    {
+        name: "United States"
+    },
+    {
+        name: "Uruguay"
+    },
+    {
+        name: "Uzbekistan"
+    },
+    {
+        name: "Venezuela"
+    },
+    {
+        name: "Vietnam"
+    },
+    {
+        name: "Western Sahara"
+    },
+    {
+        name: "Yemen"
+    },
+    {
+        name: "Zambia"
+    },
+    {
+        name: "Zimbabwe"
+    }
+]
+
+let ranCountry = '';
+let streakCtr = 0;
+
+generateFlag();
+
+function inputGuess() {
+
+    let userIn = document.getElementById('userInput').value;
+    userInLower = userIn.toLowerCase();
+    document.getElementById('userInput').value = '';
+    let strkCtrLabel = document.getElementById('streakCounter')
+    if (userInLower ===  ranCountry) {
+        document.body.style.background = 'green';
+        streakCtr += 1;
+        strkCtrLabel.innerHTML = streakCtr;
+        //salert('Correct!')
+    } else {
+        document.body.style.background = 'red';
+        streakCtr = null;
+        strkCtrLabel.innerHTML = streakCtr;
+    }
+
+    setTimeout(setBackground, 300);
+    generateFlag();
+};
+
+function setBackground() {
+
+    document.body.style.background = '#8D99AE'
+
+}
+
+function generateFlag() {
+    randCountryNum = getRand(0,188);
+    ranCountry = countryList[randCountryNum].name.toLowerCase();
+    console.log(ranCountry);
+    countryData(ranCountry);
+}
+
+async function countryData(ranCtry) {
+    const res = await fetch('https://restcountries.com/v3.1/name/'+ranCountry)
+    const data = await res.json()
+    let countryFlag = data[0].flags.svg;
+    displayFlag(countryFlag)
+}
+
+function getRand(min, max) {
+
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max-min + 1)) + min;
+
+}
+
+function displayFlag(flag) {
+
+    let flagContent = document.querySelector('.flagContent');
+    flagContent.innerHTML = '';
+    let contentDiv = document.createElement('div');
+    contentDiv.classList.add('content');
+    let cardContent = `
+        <img src="${flag}" alt="" class="cardImage">
+    `
+    contentDiv.innerHTML += cardContent;
+    flagContent.appendChild(contentDiv);
+
+}
